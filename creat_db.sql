bec536ce3c212dc4605f003700d5e954aa61af27
pjer <pjer1316@icloud.com>
2016-07-02 21:11:21 +0800

    sql correct

--- creat_db.sql
+++ creat_db.sql
@@ -54,3 +54,66 @@ BEGIN
     select * from ring_users where user_username = p_username;
 END$$
 DELIMITER ;
+
+
+#add items
+
+CREATE TABLE `ring_wish` (
+  `wish_id` int(11) NOT NULL AUTO_INCREMENT,
+  `wish_num` INT,
+  `wish_description` varchar(2000) DEFAULT NULL,
+  `wish_user_id` int(11) DEFAULT NULL,
+  `wish_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`wish_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3;
+
+
+
+DROP procedure IF EXISTS `users`.`sp_addWish`;
+
+DELIMITER $$
+USE `users`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_addWish`(
+    IN p_num INT,
+    IN p_description varchar(1000),
+    IN p_user_id bigint
+)
+BEGIN
+
+    if  (exists(SELECT 1 FROM  ring_wish WHERE ((wish_user_id = p_user_id) AND (wish_num = p_num)) ))THEN
+      SELECT 'Wish number already in wish-list';
+    ELSE
+    insert into ring_wish(
+        wish_num,
+        wish_description,
+        wish_user_id,
+        wish_date
+    )
+    values
+    (
+        p_num,
+        p_description,
+        p_user_id,
+        NOW()
+    );
+    END IF;
+END$$
+
+DELIMITER ;
+;
+
+# add wish list
+
+USE `users`;
+DROP procedure IF EXISTS `sp_GetWishByUser`;
+
+DELIMITER $$
+USE `BucketList`$$
+CREATE PROCEDURE `sp_GetWishByUser` (
+IN p_user_id bigint
+)
+BEGIN
+    select * from ring_wish where wish_user_id = p_user_id;
+END$$
+
+DELIMITER ;
